bdf241ebf210b59d3467e655c261de727424c48e
Karnik Ram <karnikram@gmail.com>
2018-11-01 23:40:58 +0530

    Add cross-validation for lsvm

--- mini-project-2/linear_svm.py
+++ mini-project-2/linear_svm.py
@@ -1,36 +1,68 @@
 import data_utils
+import sys
+import numpy as np
 from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+plt.switch_backend('agg')
 
 # Load data
 Xtr, Ytr, Xte, Yte = data_utils.load_cifar10('./cifar-10-batches-py')
-
 num_train_samples = Xtr.shape[0]
-num_validation_samples = int(num_train_samples * 0.2)
-
-mask = range(num_validation_samples)
-Xvald = Xtr[mask]
-Yvald = Ytr[mask]
-
-mask = range(num_validation_samples, num_training_samples)
-Xtr = Xtr[mask]
-Ytr = Ytr[mask]
-
-# Small developement set for quick training
-num_dev_samples = 1000
-mask = np.random.choice(Xtr.shape[0], 500, replace=False)
-Xdev = Xtr[mask]
-Ydev = Ytr[mask]
+num_test_samples = Xte.shape[0]
 
 # Sanity checks
 print('Shape of train data: {}'.format(Xtr.shape))
 print('Shape of train labels: {}'.format(Ytr.shape))
-print('Shape of validation data: {}'.format(Xvald.shape))
-print('Shape of validation labels: {}'.format(Yvald.shape))
 print('Shape of test data: {}'.format(Xte.shape))
 print('Shape of test labels: {}'.format(Yte.shape))
 
-# SVM Classifier
-clf = LinearSVC()
-clf.fit(Xtr, Ytr)
+# Small development set for quick training and testing
+num_dev_samples = 1000
+mask = np.random.choice(num_train_samples, num_dev_samples, replace=False)
+Xdev_tr = Xtr[mask]
+Ydev_tr = Ytr[mask]
 
-#print(clf.score(Xte, Yte))
+mask = np.random.choice(num_test_samples, int(num_dev_samples/2), replace=False)
+Xdev_te = Xte[mask]
+Ydev_te = Yte[mask]
+
+# Preprocessing
+
+# Unit normalization?
+# Mean normalization
+# PCA
+# LDA
+# Feature representations?
+
+# SVM Classifier
+lsvm = LinearSVC()
+best_lsvm = None
+best_mean_accuracy = 0
+
+# Hyperprameter search with cross-validation
+c_choices = [5, 10] #TODO
+num_folds = 5
+Xtr_folds = np.split(Xdev_tr,num_folds)
+Ytr_folds = np.split(Ydev_tr,num_folds)
+
+c_to_accuracies = {}
+for c in c_choices:
+    lsvm.set_params(C=c)
+    c_accuracy = np.zeros(num_folds)
+    for i in range(num_folds):
+        tr_data = np.concatenate(Xtr_folds[:i]+Xtr_folds[i+1:])
+        tr_labels = np.concatenate(Ytr_folds[:i]+Ytr_folds[i+1:])
+        lsvm.fit(tr_data,tr_labels)
+        c_accuracy[i] = lsvm.score(Xtr_folds[i],Ytr_folds[i])
+
+    if(np.mean(c_accuracy) > best_mean_accuracy):
+        best_mean_accuracy = np.mean(c_accuracy)
+        best_c = c
+        best_lsvm = deepcopy(lsvm)
+    c_to_accuracies[c] = c_accuracy
+
+for c in sorted(c_to_accuracies):
+    for accuracy in c_to_accuracies[c]:
+        print('c = {}, accuracy = {}'.format(c,accuracy))
